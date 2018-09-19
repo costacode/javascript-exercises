f3ac98eeb43e74c77fa284e567d0dcda36a22fd0
Sergio Acosta <costa@sergios-air.lan>
2018-09-19 12:52:15 +0100

    minor

--- scope.js
+++ scope.js
@@ -1,26 +1,26 @@
-// // function myLab() {
-// var coffee = "this is global coffee";
+// function myLab() {
+var coffee = "this is global coffee";
 
-// // ***************************
+// ***************************
 
-// (function tea() {
-// 	// var coffee = "sleeping";
-// })();
+(function tea() {
+	// var coffee = "sleeping";
+})();
 
-// function water() {
-// 	coffee = "global inside an IIFE, but it's not declared";
-// 	juice = "wtf";
-// }
-// water();
+function water() {
+	coffee = "global inside an IIFE, but it's not declared";
+	juice = "wtf";
+}
+water();
 
 // my array
-// const myArray = ["One", "Two", "Three", "Four"];
-// for (let i = 0; i < myArray.length; i++) {
-// 	let elem = myArray[i];
-// 	console.log(elem);
-// }
+const myArray = ["One", "Two", "Three", "Four"];
+for (let i = 0; i < myArray.length; i++) {
+	let elem = myArray[i];
+	console.log(elem);
+}
 
-// myArray.forEach(function(item) {
-// 	let elem = item;
-// 	console.log(elem);
-// });
+myArray.forEach(function(item) {
+	let elem = item;
+	console.log(elem);
+});
